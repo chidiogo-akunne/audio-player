ebf47faf3f504227dc30e1f9f5d3b49ca804659c
chidiogo-akunne <chidiogoakunne@gmail.com>
2022-08-03 00:15:45 +0100

    app constants

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+// ALL APP CONSTANTS
+
+// ALL PAGE LOGICAL CONSTANTS
+export const PAGINATION_DEFAULT: number = 15;
+export const LANGUAGE_DEFAULT: string = 'en';
+
+// APP HEADER SETTING
+export const GLOBAL_HEADER_STYLE = {
+  shadowOpacity: 0,
+  shadowOffset: { height: 0, width: 0 },
+  shadowRadius: 0,
+  elevation: 0
+};
