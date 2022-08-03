fb5cfd9f547460b04caa07bc4cc617e977f4aaad
chidiogo-akunne <chidiogoakunne@gmail.com>
2022-08-03 05:42:19 +0100

    import and export AudioScreen

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,4 +1,5 @@
 import HomeScreen from './home';
 import SplashScreen from './splash';
+import AudioScreen from './playAudio';
 
-export default { HomeScreen, SplashScreen };
+export default { HomeScreen, SplashScreen, AudioScreen };
