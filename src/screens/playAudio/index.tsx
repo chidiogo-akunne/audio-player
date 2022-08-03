06aabf21d71c195cbd1d6eda48e6a5902a198239
chidiogo-akunne <chidiogoakunne@gmail.com>
2022-08-03 20:06:19 +0100

    play, pause, seek audio

--- src/screens/playAudio/index.tsx
+++ src/screens/playAudio/index.tsx
@@ -2,8 +2,13 @@ import { Image, TouchableOpacity } from 'react-native';
 import { DataType } from 'typings/types';
 import { RFValue } from 'react-native-responsive-fontsize';
 import Ratings from '@components/starRating';
+import { Audio } from 'expo-av';
+import { useState, useEffect } from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import Slider from '@components/slider';
+import { millisecondToSeconds } from '@utils/timeFormatter';
 
-import { Container } from './styles';
+import { Container, Text } from './styles';
 
 interface PlayAudioProp {
   route: {
@@ -16,9 +21,68 @@ export default function AudioScreen(props: PlayAudioProp) {
     route: { params }
   } = props;
   const { item } = params;
+  const isFocused = useIsFocused();
+  const [sound, setSound] = useState();
+  const [isPlaying, setisPlaying] = useState(false);
+  const [state, setState] = useState({
+    isBuffering: false,
+    durationMillis: 1,
+    positionMillis: 0,
+    isSeeking: false
+  });
+
+  const { positionMillis, durationMillis } = state;
+  const positionSeconds = millisecondToSeconds(positionMillis);
+  const durationSeconds = millisecondToSeconds(durationMillis);
+
+  const loadSound = async () => {
+    const source = { uri: item.audio };
+    const playbackObject = await new Audio.Sound();
+    const status = {
+      shouldPlay: isPlaying
+    };
+    playbackObject.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+    await playbackObject.loadAsync(source, status, true);
+    setSound(playbackObject);
+  };
+
+  const Playpause = async () => {
+    isPlaying ? await sound?.pauseAsync() : await sound?.playAsync();
+    setisPlaying(!isPlaying);
+  };
+
+  const onPlaybackStatusUpdate = (status) => {
+    console.log('status4', status);
+    setState({
+      ...state,
+      isBuffering: status.isBuffering,
+      durationMillis: status.durationMillis,
+      positionMillis: status.positionMillis
+    });
+  };
+
+  const onSlidingComplete = async (value) => {
+    const currentDuration = value * durationMillis;
+    setState({
+      ...state,
+      positionMillis: Math.floor(currentDuration)
+    });
+  };
+
+  useEffect(() => {
+    loadSound();
+  }, []);
+
+  useEffect(() => {
+    if (!isFocused) {
+      setisPlaying(false);
+      sound?.unloadAsync();
+    }
+  }, [isFocused]);
+
   return (
     <Container>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={Playpause}>
         <Image
           source={{
             uri: item?.cover
@@ -31,7 +95,13 @@ export default function AudioScreen(props: PlayAudioProp) {
           }}
         />
       </TouchableOpacity>
-      <Ratings starSize={50} style={{ alignSelf: 'center' }} />
+      <Slider
+        positionMillis={positionMillis}
+        durationMillis={durationMillis}
+        onSlidingComplete={(value) => onSlidingComplete(value)}
+      />
+      <Text>{`${positionSeconds}/${durationSeconds}`}</Text>
+      <Ratings starSize={60} style={{ alignSelf: 'center' }} />
     </Container>
   );
 }
--- src/screens/playAudio/styles.ts
+++ src/screens/playAudio/styles.ts
@@ -7,3 +7,11 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.PRIMARY};
   padding: ${RFValue(30)}px ${RFValue(15)}px ${RFValue(15)}px;
 `;
+
+export const Text = styled.Text`
+  color: ${({ theme }) => theme.colors.BLACK};
+  font-size: ${({ theme }) => RFValue(theme.fonts.LARGE_SIZE + 3)}px;
+  font-family: ${({ theme }) => theme.fonts.POPPINS_SEMIBOLD};
+  text-align: center;
+  margin: ${RFValue(2)}px 0;
+`;
