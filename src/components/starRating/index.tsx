bd500efe69a1b7ca72964901d77df345faf752c0
chidiogo-akunne <chidiogoakunne@gmail.com>
2022-08-04 00:17:02 +0100

    get and store rating from local storage

--- src/components/starRating/index.tsx
+++ src/components/starRating/index.tsx
@@ -1,20 +1,46 @@
 import StarRating from 'react-native-star-rating-widget';
 import { useAppTheme } from '@app/theme';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ViewStyle } from 'react-native';
+import { DataType } from 'typings/types';
+import Storage from '@libs/storage';
+import { useIsFocused } from '@react-navigation/native';
 
 interface RatingProp {
   starSize?: number;
   style?: ViewStyle;
+  item: DataType;
+  disabled?: boolean;
 }
 
 export default function Rating(props: RatingProp) {
-  const { starSize = 30, style } = props;
+  const { starSize = 30, style, item, disabled = false } = props;
+  const isFocused = useIsFocused();
   const { colors } = useAppTheme();
   const [rating, setRating] = useState(0);
-  const ratingCompleted = (rate: number) => {
+
+  const ratingCompleted = async (rate: number) => {
+    if (disabled) return;
     setRating(rate);
+    await Storage.setStarRating({ rating: [{ tile: item, rate }] });
   };
+
+  const getStarRating = async () => {
+    const getRating = await Storage.getStarRatings();
+    if (!getRating) return;
+    const starRating = JSON.parse(getRating!);
+    const checkRating = starRating?.rating?.filter(
+      (rating: any) => rating?.tile?.title === item?.title
+    );
+    if (checkRating?.length) {
+      setRating(checkRating[0]?.rate);
+    }
+  };
+
+  useEffect(() => {
+    getStarRating();
+  }, [isFocused]);
+
   return (
     <StarRating
       rating={rating}
