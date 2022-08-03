2846918235eb56486ba951954f5d3734926341fe
chidiogo-akunne <chidiogoakunne@gmail.com>
2022-08-03 03:53:05 +0100

    fetch data from local file

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import Ratings from '@components/audioCard';
+import { useMemo } from 'react';
+import AudioCard from '@components/audioCard';
+import manifestData from '@utils/manifest.json';
+import { FlatList } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 import { Container } from './styles';
 
 export default function HomeScreen() {
+  const data = manifestData.data;
+
+  const _renderItem = useMemo(
+    () => ({ item }: any) => {
+      return <AudioCard item={item} />;
+    },
+    []
+  );
   return (
     <Container>
-      <Ratings />
+      <FlatList
+        data={data}
+        keyExtractor={(item) => {
+          return item.title;
+        }}
+        showsVerticalScrollIndicator={true}
+        renderItem={_renderItem}
+        style={{
+          width: '100%',
+          marginTop: RFValue(10)
+        }}
+        contentContainerStyle={{
+          paddingHorizontal: RFValue(15),
+          paddingBottom: RFValue(100)
+        }}
+      />
     </Container>
   );
 }
