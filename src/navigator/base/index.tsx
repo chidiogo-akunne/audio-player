bf1f92e39940d3e3c77fed367380b40df346c974
chidiogo-akunne <chidiogoakunne@gmail.com>
2022-08-03 05:42:04 +0100

    add v to base navigator

--- src/navigator/base/index.tsx
+++ src/navigator/base/index.tsx
@@ -3,7 +3,8 @@ import Screens from '@app/screens';
 import { useAppTheme } from '@app/theme';
 import { GLOBAL_HEADER_STYLE } from '@app/constants';
 import { RFValue } from 'react-native-responsive-fontsize';
-import { Text } from 'react-native';
+import { TouchableOpacity, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const BaseStack = createStackNavigator();
 
@@ -20,7 +21,7 @@ export default function BaseNavigator() {
             return (
               <Text
                 style={{
-                  color: colors.GREY_DARK,
+                  color: colors.BLACK,
                   fontSize: RFValue(fonts.LARGE_SIZE + 5),
                   fontFamily: fonts.POPPINS_MEDIUM
                 }}
@@ -31,6 +32,40 @@ export default function BaseNavigator() {
           },
           headerStyle: GLOBAL_HEADER_STYLE
         }}
+      />
+      <BaseStack.Screen
+        name="AudioScreen"
+        component={Screens.AudioScreen}
+        options={({ navigation, route }) => ({
+          headerTitle: () => {
+            return (
+              <Text
+                style={{
+                  color: colors.BLACK,
+                  fontSize: RFValue(fonts.LARGE_SIZE + 5),
+                  fontFamily: fonts.POPPINS_MEDIUM
+                }}
+              >
+                {
+                  //@ts-ignore
+                  route.params?.item?.title
+                }
+              </Text>
+            );
+          },
+          headerBackTitleVisible: false,
+          headerLeft: () => (
+            <TouchableOpacity onPress={navigation?.goBack}>
+              <Feather
+                name="chevron-left"
+                size={RFValue(30)}
+                color={colors.GREY_DARK}
+              />
+            </TouchableOpacity>
+          ),
+          headerLeftContainerStyle: { marginLeft: RFValue(15) },
+          headerStyle: GLOBAL_HEADER_STYLE
+        })}
       />
     </BaseStack.Navigator>
   );
