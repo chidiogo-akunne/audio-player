bf8e1d969583b271abe0a09da8ac93222bba57c7
chidiogo-akunne <chidiogoakunne@gmail.com>
2022-08-04 14:42:57 +0100

    mocked data

---
+++ __mocks__/mockedData.ts
@@ -0,0 +1,8 @@
+export const mockedData = {
+  title: 'Nightlife',
+  audio:
+    'https://raw.githubusercontent.com/Learnfield-GmbH/CodingChallange/master/react%20native/simple%20audio%20player/data/Nightlife.mp3',
+  cover:
+    'https://raw.githubusercontent.com/Learnfield-GmbH/CodingChallange/master/react%20native/simple%20audio%20player/data/Nightlife.png',
+  totalDurationMs: 15696
+};
