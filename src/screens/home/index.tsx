aaeccc2559d4acc9f62edd2589dc9e14c22a0b1c
chidiogo-akunne <chidiogoakunne@gmail.com>
2022-08-04 08:23:20 +0100

    fetch data using useQuery

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,13 +1,22 @@
-import { useMemo } from 'react';
+import { useMemo, Fragment } from 'react';
 import AudioCard from '@components/audioCard';
-import manifestData from '@utils/manifest.json';
 import { FlatList } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { useQuery } from 'react-query';
+import { getAudio } from '@app/network/queries';
+import { ActivityIndicator, Text } from 'react-native-paper';
+import { useAppTheme } from '@app/theme';
 
 import { Container } from './styles';
 
 export default function HomeScreen() {
-  const data = manifestData.data;
+  const { colors, fonts } = useAppTheme();
+
+  //fetch audio data using useQuery
+  const { isLoading, data } = useQuery('audio', getAudio, {
+    //auto refetch query every 1minute
+    refetchInterval: 60000
+  });
 
   const _renderItem = useMemo(
     () => ({ item }: any) => {
@@ -15,15 +24,36 @@ export default function HomeScreen() {
     },
     []
   );
+  const _renderEmptyComponent = useMemo(
+    () => () => {
+      return (
+        <Fragment>
+          {isLoading ? (
+            <ActivityIndicator
+              animating={true}
+              size={RFValue(40)}
+              color={colors.BLACK}
+              style={{ marginTop: 'auto', marginBottom: 'auto' }}
+            />
+          ) : (
+            <Text>No audio found</Text>
+          )}
+        </Fragment>
+      );
+    },
+    [isLoading]
+  );
+
   return (
     <Container>
       <FlatList
-        data={data}
+        data={data?.data}
         keyExtractor={(item) => {
           return item.title;
         }}
         showsVerticalScrollIndicator={true}
         renderItem={_renderItem}
+        ListEmptyComponent={_renderEmptyComponent}
         style={{
           width: '100%',
           marginTop: RFValue(10)
