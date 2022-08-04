1623551c0b7daafa6d1aa973c27aa03cb96db31f
chidiogo-akunne <chidiogoakunne@gmail.com>
2022-08-04 14:47:59 +0100

    play audio snapshot

---
+++ __tests__/screens/playAudio/index.tsx
@@ -0,0 +1,32 @@
+import AudioScreen from '@screens/playAudio';
+import renderer from 'react-test-renderer';
+import ThemeProvider from '@theme';
+import { ContextThemeProvider } from '@context/index';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { mockedData } from '__mocks__/mockedData';
+
+jest.useFakeTimers();
+
+// Create a react queultry client
+const queryClient = new QueryClient();
+
+const route = {
+  params: {
+    item: mockedData
+  }
+};
+
+describe('Audio Player', () => {
+  const tree = renderer.create(
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ContextThemeProvider>
+          <AudioScreen route={route} />
+        </ContextThemeProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
+  it(`renders correctly`, async () => {
+    await expect(tree).toMatchSnapshot();
+  });
+});
