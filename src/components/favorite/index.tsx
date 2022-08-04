a363bf7f8001e16936dca62860c06be57ee9f51d
chidiogo-akunne <chidiogoakunne@gmail.com>
2022-08-04 15:47:46 +0100

    store favorite in async storage

--- src/components/favorite/index.tsx
+++ src/components/favorite/index.tsx
@@ -1,9 +1,11 @@
 import { TouchableOpacity } from 'react-native';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useAppTheme } from '@app/theme';
 import { useContextTheme } from '@context/index';
 import { DataType } from 'typings/types';
+import { useIsFocused } from '@react-navigation/native';
+import Storage from '@libs/storage';
 
 interface FavoriteProp {
   size?: number;
@@ -12,26 +14,49 @@ interface FavoriteProp {
 
 export default function Favorite(props: FavoriteProp) {
   const { size = 35, item } = props;
+  const isFocused = useIsFocused();
   const { colors } = useAppTheme();
   const favoriteTheme = useContextTheme();
   const isFavorite = favoriteTheme.state.like;
   const checkIsFovarite =
     isFavorite?.favorite && isFavorite?.title === item?.title;
 
-  const handleChange = () => {
+  const handleChange = async () => {
     // update like state using context
     if (checkIsFovarite) {
       favoriteTheme.dispatch({
         type: 'UNLIKE',
         payload: { favorite: false, title: '' }
       });
+      // update like state in async storage
+      await Storage.resetFavorite();
     } else {
+      // update like state using context
+      favoriteTheme.dispatch({
+        type: 'LIKED',
+        payload: { favorite: true, title: item?.title }
+      });
+      // update like state in async storage
+      await Storage.setFavorite(item);
+    }
+  };
+
+  const getFavorite = async () => {
+    //check favorite state in storage and update context
+    const checkFavorite = await Storage.checkFavorite();
+    if (!checkFavorite) return;
+    const isFavorite = JSON.parse(checkFavorite!);
+    if (isFavorite?.title === item?.title) {
       favoriteTheme.dispatch({
         type: 'LIKED',
         payload: { favorite: true, title: item?.title }
       });
     }
   };
+
+  useEffect(() => {
+    getFavorite();
+  }, [isFocused]);
 
   return (
     <TouchableOpacity onPress={handleChange} testID={'favorite'}>
